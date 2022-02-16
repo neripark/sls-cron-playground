0a98677ba9c8c502e1b93ff5adf46d5aa04f13a1
neripark <neripark@gmail.com>
2022-02-16 20:05:52 +0900

    add endpoint for execute lighthouse

--- handler.js
+++ handler.js
@@ -1,6 +1,22 @@
 'use strict';
 
-module.exports.run = async (event, context) => {
+const { execLighthouse } = require("./src/execLighthouse");
+
+exports.run = async (event, context) => {
   const time = new Date();
   console.log(`Your cron function "${context.functionName}" ran at ${time}, neripark!`);
 };
+
+exports.lighthouse = async () => {
+  console.log(execLighthouse());
+};
+
+// esm 書けない、悔しい
+// import { execLighthouse } from "./src/lighthouse.mjs";
+// export const run = async (event, context) => {
+//   const time = new Date();
+//   console.log(`Your cron function "${context.functionName}" ran at ${time}, neripark!`);
+// };
+// export const lighthouse = async (event, context) => {
+//   console.log(execLighthouse());
+// };
---
+++ src/execLighthouse.js
@@ -0,0 +1,86 @@
+// import { Handler } from 'aws-lambda';
+
+// CommonJS形式のモジュールを使う場合は `* as` が必要
+const lighthouse = require('lighthouse');
+const log = require('lighthouse-logger');
+const chromeLauncher = require('chrome-launcher');
+
+const opts = {
+  chromeFlags: ["--headless", "--disable-gpu"],
+  //chromeFlags: ["--disable-gpu"],
+  logLevel: "info"
+};
+log.setLevel(opts.logLevel);
+
+// function launchChromeAndRunLighthouse(url: string, opts: any, config = null) {
+async function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then((chrome) => {
+    opts.port = chrome.port;
+    return lighthouse(url, opts, config).then((results) => {
+      // results.lhr はよく見るスコアリングの元データ
+      // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+      const {
+        // "time-to-first-byte": ttfb,
+        // "first-contentful-paint": fcp,
+        // "first-meaningful-paint": fmp,
+        // "speed-index": speedindex,
+        // interactive,
+        metrics,
+      } = results.lhr.audits;
+
+      return chrome.kill().then(() => ({
+        // TTFB: Math.round(ttfb.numericValue),
+        FIRST_PAINT: metrics.details.items[0].observedFirstPaint,
+        // FMP: Math.round(fmp.numericValue),
+        // FCP: Math.round(fcp.numericValue),
+        // SPEED_INDEX: Math.round(speedindex.numericValue),
+        // TTI: Math.round(interactive.numericValue),
+      }));
+    });
+  });
+}
+
+const execLighthouse = async () => {
+  return launchChromeAndRunLighthouse("https://google.com", opts).then((results) => {
+    // 一旦標準出力に表示
+    console.table(results);
+    return results;
+  });
+}
+
+
+// const lighthouse = (event) => {
+//   console.log("LOGGER:: lighthouse kicked.");
+//   return "lighthouse was kicked!!";
+
+//   // // いったん非同期処理を待たずにレスポンスさせる
+//   // execLighthouse()
+//   //   .then((value) => {
+//   //     console.log(`LOGGER::result:: ${JSON.stringify(value)}`);
+//   //   })
+//   //   .catch(e => {
+//   //     console.error(e)
+//   //   });
+
+//   // const response = {
+//   //   statusCode: 200,
+//   //   body: JSON.stringify(
+//   //     {
+//   //       message: 'Go Serverless v1.0! Your function executed successfully!',
+//   //       input: event,
+//   //     },
+//   //     null,
+//   //     2
+//   //   ),
+//   // };
+//   // // console.log(`LOGGER::response:: ${JSON.stringify(response)}`);
+
+//   // return new Promise((resolve) => {
+//   //   resolve(response)
+//   // })
+// }
+
+exports.execLighthouse = (event) => {
+  console.log("LOGGER:: lighthouse kicked.");
+  return "lighthouse was kicked!!";
+}
